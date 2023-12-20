1f61bae023721a52921465a5245453c33f69486b
李征 <lizheng@steedos.com>
2023-12-20 01:46:16 +0000

    软件包微服务启动调整

--- steedos-packages-expense/expense/package.service.js
+++ steedos-packages-expense/expense/package.service.js
@@ -1,11 +1,2 @@
-/*
- * @Author: 孙浩林 sunhaolin@steedos.com
- * @Date: 2023-10-27 12:51:51
- * @LastEditors: 孙浩林 sunhaolin@steedos.com
- * @LastEditTime: 2023-10-27 15:00:09
- * @FilePath: /steedos-ee-gitlab/steedos-packages-oa/gw/package.service.js
- * @Description: 
- */
-
 const service = require("./dist/index.js");
 module.exports = service;
