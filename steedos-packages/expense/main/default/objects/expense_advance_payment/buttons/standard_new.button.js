9b94a783f3dd727b3240003b2a4804db04ef6f4d
易仕川 <yishichuan@steedos.com>
2024-01-12 06:16:35 +0000

    报销单记录详情页的借款相关表隐藏新建按钮 #117

--- steedos-packages/expense/main/default/objects/expense_advance_payment/buttons/standard_new.button.js
+++ steedos-packages/expense/main/default/objects/expense_advance_payment/buttons/standard_new.button.js
@@ -1,8 +1,13 @@
 module.exports = {
-    standard_new: function (object_name, record_id) {
+  standard_new: function (object_name, record_id) {
 
-    },
-    standard_newVisible: function (object_name, record_id, record_permissions, record) {
-      return false
-    }
+  },
+  standard_newVisible: function (object_name, record_id, record_permissions, record) {
+      const sizeUp = true;
+      var status = null
+      if(record.__super.__super._master){
+              sizeUp = false;
+      }
+      return sizeUp;
+  }
 }
--- steedos-packages/invoice/main/default/objects/invoices/buttons/standard_new.button.js
+++ steedos-packages/invoice/main/default/objects/invoices/buttons/standard_new.button.js
@@ -3,10 +3,13 @@ module.exports = {
 
     },
     standard_newVisible: function (object_name, record_id, record_permissions, record) {
-        const status = record.__super.__super._master.record.instance_state;
         const sizeUp = true;
-        if (status == "approved") {
-            sizeUp = false;
+        var status = null
+        if(record.__super.__super._master){
+            status = record.__super.__super._master.record.instance_state;
+            if (status && status == "approved") {
+                sizeUp = false;
+            }
         }
         return sizeUp;
     }
