18ad57c7d16d104ab2ab704ee9365f37aa5b02c1
易仕川 <yishichuan@steedos.com>
2024-01-25 07:24:40 +0000

    【预收款】增加“核销”、“退款”按钮 #171

--- steedos-packages/finance/main/default/objects/finance_refund/buttons/standard_new.button.js
+++ steedos-packages/finance/main/default/objects/finance_refund/buttons/standard_new.button.js
@@ -1,6 +1,6 @@
 module.exports = {
   standard_newVisible: function (object_name, record_id, record_permissions, record) {
-    if(record.relatedKey=="contract_receive_detail" || record.relatedKey=="finance_pre_receive"){
+    if(record.relatedKey=="finance_pre_receive"){
       return false;
     }
       return true;
