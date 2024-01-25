f2f87228e7948b683b16d25e5e1e348497de572d
易仕川 <yishichuan@steedos.com>
2024-01-25 10:23:50 +0000

    收款审批通过后还是会有发起审批按钮 #173

--- steedos-packages/finance/main/default/objects/finance_receive/buttons/standard_approve.button.js
+++ steedos-packages/finance/main/default/objects/finance_receive/buttons/standard_approve.button.js
@@ -1,7 +1,8 @@
 module.exports = {
   standard_approveVisible: function (object_name, record_id, record_permissions, record) {
-    let is_active = record.__super.is_active
-    if(is_active){
+    let is_active = record.__super.is_active;
+    let instance_state = record.__super.instance_state;
+    if(is_active || instance_state != undefined){
       return false;
     }
       return true;
