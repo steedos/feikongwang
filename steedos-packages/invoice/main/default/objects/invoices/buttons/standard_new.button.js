2117efdc0f5c86977abe191fe5e0e144061b18b3
李征 <lizheng@steedos.com>
2024-02-22 03:54:21 +0000

    借款模块按钮权限调整

--- steedos-packages/invoice/main/default/objects/invoices/buttons/standard_new.button.js
+++ steedos-packages/invoice/main/default/objects/invoices/buttons/standard_new.button.js
@@ -1,12 +1,8 @@
 module.exports = {
     standard_newVisible: function (object_name, record_id, record_permissions, record) {
         var sizeUp = true;
-        var status = null
-        if(record.__super.__super._master){
-            status = record.__super.__super._master.record.instance_state;
-            if (status && status == "approved") {
+        if(record.__super.__super._master && record.__super.__super._master.record.locked){
                 sizeUp = false;
-            }
         }
         return sizeUp;
     }
