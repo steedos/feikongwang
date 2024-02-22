bc40c432a459792665773c5f52bfe504951a6d0e
李征 <lizheng@steedos.com>
2024-02-22 08:24:23 +0000

    应收模块按钮显隐控制

--- steedos-packages/sales/main/default/objects/contract_invoice_detail/buttons/standard_new.button.js
+++ steedos-packages/sales/main/default/objects/contract_invoice_detail/buttons/standard_new.button.js
@@ -2,6 +2,8 @@ module.exports = {
   standard_newVisible: function (object_name, record_id, record_permissions, record) {
     if(record.relatedKey=="contract"){
       return false;
+    }else if(record.relatedKey=="finance_invoice" && record.__super.__super.locked){
+      return false;
     }
       return true;
   }
--- steedos-packages/sales/main/default/objects/contract_receive_detail/buttons/standard_new.button.js
+++ steedos-packages/sales/main/default/objects/contract_receive_detail/buttons/standard_new.button.js
@@ -2,6 +2,8 @@ module.exports = {
   standard_newVisible: function (object_name, record_id, record_permissions, record) {
     if(record.relatedKey=="contract"){
       return false;
+    }else if(record.relatedKey=="finance_receive" && record.__super.__super.locked){
+      return false;
     }
       return true;
   }
