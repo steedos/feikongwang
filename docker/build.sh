418f522db2445eaeffb41d5701b6fe96ea309157
chenzhipei <chenzhipei@hotoa.com>
2024-02-23 02:15:37 +0000

    docker build 0.0.6

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export FEIKONGWANG_VERSION=0.0.4
+export FEIKONGWANG_VERSION=0.0.6
 echo "#########################################################################"
 echo "feikongwang version: ${FEIKONGWANG_VERSION}"
 echo "#########################################################################"
