da4b291a250678e2149302cbfda75b9589b0bde5
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2025-08-22 16:48:32 +0800

    3.0 直接运行

--- steedos.config.js
+++ steedos.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 孙浩林 sunhaolin@steedos.com
  * @Date: 2023-10-27 12:51:52
- * @LastEditors: 孙浩林 sunhaolin@steedos.com
- * @LastEditTime: 2023-10-27 15:13:47
+ * @LastEditors: baozhoutao@steedos.com
+ * @LastEditTime: 2024-12-09 15:15:42
  * @FilePath: /steedos-ee-gitlab/steedos.config.js
  * @Description: 
  */
@@ -13,19 +13,14 @@ module.exports = {
 	namespace: "steedos",
 	// Default log level for built-in console logger. It can be overwritten in logger options above.
 	// Available values: trace, debug, info, warn, error, fatal
-	logLevel: "warn",
+	logLevel: process.env.STEEDOS_LOG_LEVEL || "warn",
 
 	// Called after broker started.
 	started(broker) {
-		
 	},
     
     settings: {
-        // public: {
-		// 	workflow: {
-        //     	instance_allow_distribute: true
-		// 	}
-        // }
+       
     }
 
 };
